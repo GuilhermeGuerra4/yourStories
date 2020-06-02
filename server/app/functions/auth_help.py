3ede78454c683459f61adcd5b5dfc94dae8df354
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2020-06-02 22:30:11 +0100

    add function verify_login

--- server/app/functions/auth_help.py
+++ server/app/functions/auth_help.py
@@ -1,6 +1,14 @@
 import time
+import hashlib
+from app.db import db
+from app.models.tables import User
 
 def token_generator(user_email):
 	salt = '?Dh:|^dRS3IX,#?sVE~di:V"m1-h8;kE4!I*+U~Xa0B048DgNowUPJO09809qwd~A|g_'+str(time.time())
 	token = hashlib.sha256((user_email+salt).encode('ascii')).hexdigest()
 	return token
+
+def verify_login(token):
+	is_in_db = User.query.filter_by(token=token).count()
+	if is_in_db > 0:
+		return True
