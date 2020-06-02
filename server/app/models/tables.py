9f97d16dabeee4ab407cdbfdeb11d6f0b1be088c
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2020-06-02 18:15:49 +0100

    add field google_id

--- server/app/models/tables.py
+++ server/app/models/tables.py
@@ -3,9 +3,10 @@ from app.db import db
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	token = db.Column(db.String(80), unique=True, nullable=False)
+	google_id = db.Column(db.String(500), unique=True, nullable=False)
 	full_name = db.Column(db.String(50), nullable=False)
 	email = db.Column(db.String(50), nullable=False)
-	birthdate = db.Column(db.Date(), nullable=False)
+	birthdate = db.Column(db.Date(), nullable=True)
 	photo = db.Column(db.String(100), nullable=False)
 	locale = db.Column(db.String(5), nullable=False)
 	ip = db.Column(db.String(20), nullable=False)
