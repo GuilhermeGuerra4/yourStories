3a2ca7fe3c9e62ff021baae8839a7c93649536ef
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2020-06-03 21:57:05 +0100

    include get_sketch

--- server/main.py
+++ server/main.py
@@ -4,5 +4,5 @@ from app.config import *
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-from app.controllers import get_stories, signin
+from app.controllers import get_stories,get_sketch, signin
 app.run(host="0.0.0.0", port=3000, debug=True)
