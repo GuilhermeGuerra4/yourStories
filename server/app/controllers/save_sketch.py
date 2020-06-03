2c5b4d4a9aa03f582b48632e75d6755c727e384b
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2020-06-03 22:44:53 +0100

    add controller save_sketch

---
+++ server/app/controllers/save_sketch.py
@@ -0,0 +1,36 @@
+from __main__ import app
+from app.db import db
+from app.models.tables import User, Story
+from app.functions.auth_help import verify_login
+from app.functions.user import get_user_by_token
+from flask import request
+import json, time
+
+@app.route('/save_sketch', methods=['POST'])
+def save_sketch():
+	response = {'status': False, 'message': ''}
+	
+	if not 'token' in request.form or not 'text' in request.form:
+		response['message'] = 'bad request'
+	else:
+		text = request.form['text']
+		token = request.form['token']
+		is_signed = verify_login(token)
+
+		if is_signed == False:
+			response['message'] = 'not authentificated'
+		else:
+			user = get_user_by_token(token)
+			story = Story.query.filter_by(publisher_id=user.id, status='in_sketch').with_entities(Story.id, Story.title, Story.text).first()
+			response['status'] = True
+			if story == None:
+				timestamp_now = str(time.time())
+				new_story = Story(id=False, title="", text=text, preview="", tags="", datetime_created=timestamp_now, last_update=timestamp_now, status="in_sketch", publisher_id=user.id)
+				db.session.add(new_story)
+				response['message'] = 'story created'
+			else:
+				db.session.query(Story).filter(Story.id == story.id).update({Story.text:text})
+				response['message'] = 'story saved'
+			db.session.commit()
+
+	return(json.dumps(response))
