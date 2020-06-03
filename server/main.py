1ccb12c04b136645b50391b48f522e7efac50df2
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2020-06-03 22:45:11 +0100

    include controller save_sketch

--- server/main.py
+++ server/main.py
@@ -4,5 +4,6 @@ from app.config import *
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
-from app.controllers import get_stories,get_sketch, signin
+from app.controllers import get_stories,signin
+from app.controllers import get_sketch, save_sketch
 app.run(host="0.0.0.0", port=3000, debug=True)
