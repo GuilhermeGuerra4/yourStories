122fc099c43dd5ed0f6e69a8c4fbc27498c59376
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2020-06-04 13:24:30 +0100

    include publish_sketch

--- server/main.py
+++ server/main.py
@@ -5,5 +5,5 @@ from app.config import *
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 from app.controllers import get_stories,signin
-from app.controllers import get_sketch, save_sketch
+from app.controllers import get_sketch, save_sketch, publish_sketch
 app.run(host="0.0.0.0", port=3000, debug=True)
