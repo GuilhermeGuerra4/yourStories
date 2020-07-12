e7c39f2a94e299567dc47a3c32ad544d6a3d72ab
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2020-07-12 11:16:05 +0100

    add loading stories and refresh

--- client/yourStories/src/screens/HomeScreen.js
+++ client/yourStories/src/screens/HomeScreen.js
@@ -1,50 +1,135 @@
-import React, {useContext} from "react";
-import {Components, View, Text, TouchableOpacity, ScrollView, StyleSheet} from "react-native";
+import React, {useContext, useState, useEffect, useStateCallback} from "react";
+
+import {Components,
+	View, Text, TouchableOpacity, FlatList,
+	StyleSheet, Dimensions, ActivityIndicator} from "react-native";
+
 import Header from '../components/header';
 import Story from '../components/story';
+import Loading from "../components/loading";
+import StatusMessage from "../components/statusMessage";
+import List from "../components/list";
+
 import {AuthContext} from "../components/context";
 import AsyncStorage from '@react-native-community/async-storage';
+import api from "../libraries/axios";
+import {primaryColor} from "../assets/colors";
 
 export default function HomeScreen({navigation}){
-	
+
+
+	const [stories, setStories] = useState([]);
+	const [storiesCount, setStoriesCount] = useState(0);
+	const [token, setToken] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+	const [page, setPage] = useState(1);
+	const [isEmpty, setIsEmpty] = useState(true);
+	const [isRefreshing, setIsRefreshing] = useState(false);
+	const [photo, setPhoto] = useState(null);
+	const [status, setStatus] = useState('isLoading');
+
 	function goTo(){
 		navigation.navigate('ConfigurationsScreen');
 	}
 
+
 	function loadStories(){
-		
+		console.log("LOADING ITEMS: "+page);
+		if(isLoading == false){
+			setIsLoading(true);
+			if(token != null){
+				api.get('/get_stories/'+token+'/'+page+'/').then((res) => {
+					if(res.data.status == true){
+						if(res.data.size != 0 && res.data.payload != undefined){
+							setStoriesCount(storiesCount => storiesCount+res.data.size);
+							setPage(page => page+1);
+							setStories([...stories, ...res.data.payload]);
+							setIsEmpty(false);
+							setIsLoading(false);
+							setStatus('loaded');
+						}
+						else{
+							setStatus('server_error');
+						}
+					}
+				}).catch(() => {
+					setStatus('connection_error');
+				});
+			}
+		}
+	}
+	
+	useEffect(() => {
+		console.log("componentDidMount");
+		AsyncStorage.multiGet(['token', 'photo'], (err, stores) => {
+			stores.map((result, i, store) => {
+				let key = store[i][0];
+				let value = store[i][1];
+				if(key == 'token'){
+					setToken(value);
+				}
+				else if(key == 'photo'){
+					setPhoto(value);
+				}
+			});
+		});
+	}, []);
+
+	useEffect(() => {
+		console.log("tokenLoaded");
+		if(token != null && storiesCount == 0){
+			loadStories();
+		}
+	}, [token]);
+
+	function renderStory(story){
+		return (
+			<Story 
+				navigation={navigation}
+				story_id={story.item.id}
+				preview={story.item.preview}
+				title={story.item.title} />
+		)
 	}
 
-	const authManager = useContext(AuthContext);
-	const signout = async () => {
-		authManager.signOut();
-	};
+	function handleRefresh(){
+		if(isRefreshing == false){
+			setIsRefreshing(true);
+			setStories(stories => []);
+			setStoriesCount(0);
+			setPage(1);				
+		}	
+	}
+	useEffect(() => {
+		if(isRefreshing == true && storiesCount == 0 && stories.length == 0){
+			setIsRefreshing(false);	
+			loadStories();
+			console.log("refresh");
+		}
+	}, [isRefreshing]);
 
-	var elements = [1,2,3,4,5,6,7,8,9,10,11,12,13];
-
-	AsyncStorage.getItem('token').then((res) => {
-		console.log('token: ', res);
-	});
+	function getContent(){
+		if(status == 'isLoading'){
+			return(<Loading />);
+		}
+		else if(status == 'connection_error' || status == 'server_error'){
+			return(<StatusMessage />);
+		}
+		else if(status == 'loaded'){
+			return(
+				<List 
+					stories={stories}
+					renderStory={renderStory}
+					isRefreshing={isRefreshing}
+					handleRefresh={handleRefresh}/>
+				);
+		}
+	}
 
 	return(
 			<View>
-				<Header authManager={authManager} navigation={navigation} profile_image="https://lh3.googleusercontent.com/a-/AOh14GidSZ_f_dvKAOOss_hcTS66iWhhTvc7cDXQty_y=s96-c" />
-
-				<View>
-					<ScrollView style={style.scroll}>
-					
-					{elements.map((value, index) => {
-        				return <Story navigation={navigation} key={value} story_id={value} title={"title "+value} preview={"preview" + value} />
-      				})}
-
-					</ScrollView>
-				</View>
+				<Header navigation={navigation} profile_image={photo} />
+				{getContent()}
 			</View>
 		)
 }
-
-const style = StyleSheet.create({
-	scroll: {
-		marginBottom: 100,
-	},
-});
