42615ecfa661ad60a3ba17d568dad59fe5a049fd
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2020-07-12 14:24:40 +0100

    add optional button with callback

--- client/yourStories/src/components/statusMessage.js
+++ client/yourStories/src/components/statusMessage.js
@@ -1,19 +1,45 @@
 import React from "react";
-import {Text, View, StyleSheet} from "react-native";
+import {Text, View, StyleSheet, TouchableOpacity} from "react-native";
+import {primaryColor} from "../assets/colors";
 
 export default function StatusMessage(props){
+
+	function renderCallbackButton(){
+		return(
+			<TouchableOpacity onPress={props.buttonCallback} style={styles.button}>
+				<Text style={styles.buttonText}>Try again</Text>
+			</TouchableOpacity>
+		);
+	}
+
 	return(
-		<View>
+		<View style={styles.container}>
 			<Text style={styles.status}>{props.message}</Text>
+			{props.hasCallback ? renderCallbackButton() : false}
 		</View>
 	)
 }
 
 const styles = StyleSheet.create({
+	container: {
+		marginTop: 40,
+		alignItems: "center",
+		justifyContent: "center",
+	},
 	status: {
-		alignSelf: "center",
-		fontSize: 17,
-		color: "#333",
+		fontSize: 19,
+		color: "#555",
 		marginTop: 20,
+	},
+	button: {
+		alignSelf: "center",
+		backgroundColor: primaryColor,
+		borderRadius: 3,
+		padding: 8,
+		marginTop: 20,
+	},
+	buttonText: {
+		fontSize: 18,
+		color: "#fff",
 	},
 });
