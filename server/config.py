eacade35dc775bdadcd037283ea51fb12d32ff3d
Guilherme Guerra <guilhermeguerra12345@gmail.com>
2020-07-13 12:02:45 +0100

    fix mysql codification for emojis

--- server/config.py
+++ server/config.py
@@ -1,4 +1,5 @@
 GOOGLE_API_CLIENT_ID = '565626337947-417t02hmmpjpifltvd0fhqhhgs59su4v.apps.googleusercontent.com'
 GOOGLE_API_ENDPOINT = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/your_stories'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/your_stories?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+MYSQL_DATABASE_CHARSET = 'utf8mb4'
