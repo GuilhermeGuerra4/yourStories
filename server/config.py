c5c5bd011ebd09d70693ebe57089f5522b21d7d5
guilhermeguerra4 <guilhermeguerra12345@gmail.com>
2020-07-20 13:17:32 +0100

    change certificate

--- server/config.py
+++ server/config.py
@@ -1,4 +1,4 @@
-GOOGLE_API_CLIENT_ID = '565626337947-417t02hmmpjpifltvd0fhqhhgs59su4v.apps.googleusercontent.com'
+GOOGLE_API_CLIENT_ID = '123449495234-hvoe37lfffoqifs8hcb1c3m4umc851s4.apps.googleusercontent.com'
 GOOGLE_API_ENDPOINT = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/your_stories?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
