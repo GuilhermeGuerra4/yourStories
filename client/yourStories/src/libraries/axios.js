22175bbf17bd95282e0673fae2cefd6800d96ca3
guilhermeguerra4 <guilhermeguerra12345@gmail.com>
2020-07-28 15:14:42 +0100

    add server debug option

--- client/yourStories/src/libraries/axios.js
+++ client/yourStories/src/libraries/axios.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 
-let api = axios.create({
-  baseURL: 'http://85.242.4.235:3000',
-  timeout: 10000,
-});
+var debug = false;
+let api;
 
+if(debug == true){
+	api = axios.create({
+	  baseURL: 'http://192.168.1.64:3000',
+	  timeout: 10000,
+	});
+}
+else{
+	api = axios.create({
+	  baseURL: 'http://85.242.4.235:3000',
+	  timeout: 10000,
+	});
+}
 export const setClientToken = token => {
   APIKit.interceptors.request.use(function(config) {
     config.headers.Authorization = `Bearer ${token}`;
