c08e0de2073e00737c8b53466e2a7b562edbf99f
guilhermeguerra4 <guilhermeguerra12345@gmail.com>
2020-07-29 16:05:57 +0100

    create last time online function

--- server/app/functions/user.py
+++ server/app/functions/user.py
@@ -1,8 +1,12 @@
 from app.db import db
 from app.models.tables import User
+import time
 
 def get_user_by_token(token):
 	query = User.query.filter_by(token=token).first()
 	return query
 
 
+def update_last_time_online(token):
+	db.session.query(User).filter(User.token == token).update({User.last_datetime_online})
+	db.session.commit()
